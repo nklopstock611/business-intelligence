90ec27a52c2d94cf090d626a5265ad8c75490650
Juan Camilo <j.fallag@uniandes.edu.co>
2023-04-13 09:18:02 -0500

    modelo y avances en el API

--- practicaAPI/PredictionModel.py
+++ practicaAPI/PredictionModel.py
@@ -1,10 +1,13 @@
 from joblib import load
+import pandas as pd
 
 class Model:
 
     def __init__(self,columns):
         self.model = load("assets/modelo.joblib")
 
-    def make_predictions(self, data):
-        result = self.model.predict(data)
-        return result
+def make_predictions(self, data):
+    result = self.model.predict(data)
+    df = pd.DataFrame(result, columns=['prediction'])
+    return df
+    
